f3ffc116cc85124745743dcf441f0576cbb16c13
YanaOw <y.ovchinnikova08@gmail.com>
2026-06-14 21:22:41 +0200

    tochnovse

---
+++ kursovayaApi/Controllers/UomController.cs
@@ -0,0 +1,55 @@
+﻿using kursovayaApi.Models;
+using kursovayaApi.Models.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kursovayaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UomController : ControllerBase
+    {
+        private readonly KursovayaContext db;
+        private readonly UomService service;
+
+        public UomController(KursovayaContext db)
+        {
+            this.db = db;
+            service = new UomService(db);
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UnitsOfMeasure>>> GetAll()
+        {
+            return Ok(await service.GetAll());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UnitsOfMeasure>> GetById(int id)
+        {
+            var uom = service.Get(id).Result;
+            return uom == null? NotFound(new {message = "не найдено"}) : Ok(uom);
+        }
+        [HttpPost]
+        public async Task<ActionResult<UnitsOfMeasure>> Create([FromBody] UnitsOfMeasure uom)
+        {
+            if (service.Create(uom))
+                return CreatedAtAction(nameof(GetById), new { id = uom.UomId }, uom);
+            return BadRequest();
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UnitsOfMeasure>> Update(int id, [FromBody] UnitsOfMeasure uom)
+        {
+            if (uom.UomId != id) return BadRequest();
+            if (service.Update(id, uom))
+                return Ok(uom);
+            return NotFound();
+        }
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+            if (service.Delete(id))
+                return NoContent();
+            return NotFound();
+        }
+    }
+}
---
+++ kursovayaApi/Models/Services/UomService.cs
@@ -0,0 +1,60 @@
+﻿using kursovayaApi.Models.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace kursovayaApi.Models.Services
+{
+    public class UomService : AbstractionService, ICommonService<UnitsOfMeasure, int>
+    {
+        private readonly KursovayaContext db;
+
+        public UomService(KursovayaContext db)
+        {
+            this.db = db;
+        }
+
+        public bool Create(UnitsOfMeasure model)
+        {
+            bool result = DoAction(() =>
+            {
+                db.UnitsOfMeasures.Add(model);
+                db.SaveChanges();
+            });
+            return result;
+        }
+
+        public bool Delete(int id)
+        {
+            bool result = DoAction(() =>
+            {
+                UnitsOfMeasure uom = db.UnitsOfMeasures.FirstOrDefault(p => p.UomId == id)!;
+                db.UnitsOfMeasures.Remove(uom);
+                db.SaveChanges();
+            });
+            return result;
+        }
+
+        public async Task<UnitsOfMeasure> Get(int id)
+        {
+            var res = await db.UnitsOfMeasures.FirstOrDefaultAsync(p => p.UomId == id)!;
+            return res!;
+        }
+
+        public async Task<IEnumerable<UnitsOfMeasure>> GetAll()
+        {
+            return await db.UnitsOfMeasures.ToListAsync();
+        }
+
+        public bool Update(int id, UnitsOfMeasure model)
+        {
+            bool result = DoAction(() =>
+            {
+                UnitsOfMeasure uom = db.UnitsOfMeasures.FirstOrDefault(p => p.UomId == id)!;
+                uom.Symbol = model.Symbol;
+                uom.Name = model.Name;
+                db.UnitsOfMeasures.Update(uom);
+                db.SaveChanges();
+            });
+            return result;
+        }
+    }
+}
